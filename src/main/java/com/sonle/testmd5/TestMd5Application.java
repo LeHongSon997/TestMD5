f0641bc7dec0ba4f9a7b429fac90ce80bef297b4
LeHongSon <lehongson.05031997@gmail.com>
2022-03-03 09:02:53 +0700

    up

---
+++ src/main/java/com/sonle/testmd5/TestMd5Application.java
@@ -0,0 +1,13 @@
+package com.sonle.testmd5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMd5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMd5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sonle/testmd5/TestMd5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonle.testmd5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMd5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
